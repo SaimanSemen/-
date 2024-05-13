c19ee20ef1d3d27a4a1edcb18ba9f0620914a4d8
kyzbe <kyzberov3632007@gmail.com>
2024-05-13 13:20:22 +0700

    2

--- lesson_2/esson_2_task_0.py
+++ lesson_2/esson_2_task_0.py
@@ -1,3 +1,9 @@
-my_age = input("ваш возраст? ") 
+#my_age = input("ваш возраст? " ) 
+
+#print ("Ваш возварт" , my_age )
+
+
+my_age = input("ваш возраст? " ) 
+my_age = int (+1)
 
 print ("Ваш возварт" , my_age )
