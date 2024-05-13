2c22abb46421fa662d9abb05289e9a9c603e5b2d
kyzbe <kyzberov3632007@gmail.com>
2024-05-13 14:37:20 +0700

    2

--- lesson_2/lesson_2_task_5.py
+++ lesson_2/lesson_2_task_5.py
@@ -1,6 +1,6 @@
-month_to_season = (1)
-month_to_season = (2)
-month_to_season = (3)
-month_to_season = (4)
+month_to_season = (12,1,2)== "зима"
+month_to_season = (3,4,5)== "весна"
+month_to_season = (6,7,8)== "лето"
+month_to_season = (9,10,11)=="осень"
 
-input "1"
+print= (month_to_season "input" )
