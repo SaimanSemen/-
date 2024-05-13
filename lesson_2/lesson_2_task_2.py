f11d1c7098ae1fcf3f9e6801a67e99e527e2f6cd
kyzbe <kyzberov3632007@gmail.com>
2024-05-13 13:30:33 +0700

    2

---
+++ lesson_2/lesson_2_task_2.py
@@ -0,0 +1,10 @@
+def is year_leap(year):
+    if year % 4-0:
+      return True
+    else:
+      return False
+
+year int(input("Введите год: "))
+leap year is_year_leap(year)
+
+print(f"год (year): (leap_year)")
