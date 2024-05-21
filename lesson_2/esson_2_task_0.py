fe96a01f23d77e6e551d9862cfb60594238b0c62
kyzbe <kyzberov3632007@gmail.com>
2024-05-21 14:23:08 +0700

    верное

--- lesson_2/esson_2_task_0.py
+++ lesson_2/esson_2_task_0.py
@@ -1,12 +1,4 @@
-#my_age = input("ваш возраст? " ) 
-
-#print ("Ваш возварт" , my_age )
-
-
-my_age = input("ваш возраст? " ) 
-my_age = int (+1)
-
-print ("Ваш возварт" , my_age )
-
-
-#НЕ МОГУ ПОНЯТЬ ЧЕ ДЕЛАТЬ
+my_age = int(input("Введите ваш возраст:")) 
+print("Ваш возраст:", my_age)
+my_age = my_age + 1
+print("Ваш возраст:", my_age)
