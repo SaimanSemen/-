0c73083633f0748307fd1301ef7581b5a194f5c6
kyzbe <kyzberov3632007@gmail.com>
2024-05-25 13:31:18 +0700

    ok

--- lesson_2/lesson_2_task_3.py
+++ lesson_2/lesson_2_task_3.py
@@ -1,6 +1,7 @@
-def area_square(n):      
-    area = pow(n,2)    
-    return area
-    
-num=float(input("Enter number") )
-print("Sum of digits",area_square(num))
+import math
+def square(side):
+    return math.ceil((side*side))
+print(square(3))
+print(3*3)
+print(square(1.1))
+print(1.1*1.1)
