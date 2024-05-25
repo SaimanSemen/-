58afcccadd80dc7d26e5d64b102462370dfd3391
kyzbe <kyzberov3632007@gmail.com>
2024-05-25 13:45:53 +0700

    ok

--- lesson_2/lesson_2_task_5.py
+++ lesson_2/lesson_2_task_5.py
@@ -1,6 +1,17 @@
-month_to_season = (12,1,2)== "зима"
-month_to_season = (3,4,5)== "весна"
-month_to_season = (6,7,8)== "лето"
-month_to_season = (9,10,11)=="осень"
+def month_to_season(month):
+    if month in [12, 1, 2]:
+         return "Зима"
+    elif month in [3, 4, 5]:
+         return "Bесна"
+    elif month in [6, 7, 8]:
+         return "Лето"
+    elif month in [9, 10, 11]:
+         return "Ocень"
+    else:
+         return "Неверный номер месяца"
 
-print= (month_to_season "input" )
+print(month_to_season(1))
+print(month_to_season(5))
+print(month_to_season(6))
+print(month_to_season(10))
+print(month_to_season(15))
